3d8a15cc98cf8a853db2cc86125ef0db7bf3a6da
sootyian <ian@northeastsooty.co.uk>
2026-05-02 23:55:38 +0000

    Uploaded images

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,5 +1,4 @@
 import gulp from 'gulp';
-import imageResize from 'gulp-image-resize';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import uglify from 'gulp-uglify';
@@ -7,77 +6,108 @@ import rename from 'gulp-rename';
 import filter from 'gulp-filter';
 import path from 'path';
 import del from 'del';
+import sharp from 'sharp';
 
 const sass = gulpSass(dartSass);
+
 const cssSourceGlob = './assets/sass/**/*.scss';
 const cssOutputDir = './assets/css';
+
 const generatedCssFiles = [
     `${cssOutputDir}/custom.min.css`,
     `${cssOutputDir}/main.min.css`,
     `${cssOutputDir}/noscript.min.css`
 ];
 
+/* ---------------- CLEAN ---------------- */
+
 gulp.task('delete', function () {
-    return del(['images/*.*']);
+    return del(['images/fulls/**/*', 'images/thumbs/**/*']);
 });
 
-gulp.task('resize-images', function () {
-    return gulp.src('images/*.*')
-        .pipe(imageResize({
-            width: 1024,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('images/fulls'))
-        .pipe(imageResize({
-            width: 512,
-            imageMagick: true
-        }))
-        .pipe(gulp.dest('images/thumbs'));
-});
-
-// clear previously generated css
 gulp.task('clean-css', function () {
     return del(generatedCssFiles);
 });
 
-// compile scss to css
-gulp.task('sass', gulp.series('clean-css', function compileSass() {
-    return gulp.src(cssSourceGlob)  // Target all .scss files
-        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(rename(function (path) {
-            path.basename += '.min';  // Append .min to the output filename
-        }))
-        .pipe(gulp.dest(cssOutputDir));  // Output to the CSS directory
-}));
+/* ---------------- IMAGES (SHARP + AUTO CONVERT) ---------------- */
 
-// watch changes in scss files and run sass task
-gulp.task('sass:watch', function () {
-    gulp.watch('./assets/sass/**/*.scss', gulp.series('sass'));
+gulp.task('resize-images', async function () {
+    const files = await new Promise((resolve, reject) => {
+        const out = [];
+
+        gulp.src('images/*.*')
+            .on('data', file => out.push(file.path))
+            .on('end', () => resolve(out))
+            .on('error', reject);
+    });
+
+    await Promise.all(
+        files.map(async (filePath) => {
+            const fileName = path.parse(filePath).name + '.jpg';
+
+            try {
+                const input = sharp(filePath, {
+                    failOnError: false,
+                    unlimited: true
+                });
+
+                const buffer = await input
+                    .rotate()
+                    .jpeg({ quality: 85 })
+                    .toBuffer();
+
+                await sharp(buffer)
+                    .resize({ width: 1024 })
+                    .jpeg({ quality: 85 })
+                    .toFile(`images/fulls/${fileName}`);
+
+                await sharp(buffer)
+                    .resize({ width: 512 })
+                    .jpeg({ quality: 80 })
+                    .toFile(`images/thumbs/${fileName}`);
+
+            } catch (err) {
+                console.warn(`Skipping broken image: ${filePath}`);
+            }
+        })
+    );
 });
 
-// minify js
+/* ---------------- SASS ---------------- */
+
+gulp.task('sass', gulp.series('clean-css', function compileSass() {
+    return gulp.src(cssSourceGlob)
+        .pipe(
+            sass({ outputStyle: 'compressed' })
+                .on('error', sass.logError)
+        )
+        .pipe(rename(function (filePath) {
+            filePath.basename += '.min';
+        }))
+        .pipe(gulp.dest(cssOutputDir));
+}));
+
+gulp.task('sass:watch', function () {
+    gulp.watch(cssSourceGlob, gulp.series('sass'));
+});
+
+/* ---------------- JS ---------------- */
+
 gulp.task('minify-js', function () {
     return gulp.src('./assets/js/**/*.js')
-        .pipe(filter(function (file) {
-            const filePath = file.path;
-            const basename = path.basename(filePath, '.js');
-            
-            // Skip files that are already minified
-            return !basename.endsWith('.min');
-        }))
+        .pipe(filter(file => !file.path.endsWith('.min.js')))
         .pipe(uglify())
-        .pipe(rename(function (path) {
-            path.basename += '.min';
-            path.extname = '.js';
+        .pipe(rename(function (filePath) {
+            filePath.basename += '.min';
+            filePath.extname = '.js';
         }))
         .pipe(gulp.dest('./assets/js'));
 });
 
-// build task
+/* ---------------- TASKS ---------------- */
+
 gulp.task('build', gulp.series('sass', 'minify-js'));
 
-// resize images
 gulp.task('resize', gulp.series('delete', 'resize-images'));
 
-// default task
 gulp.task('default', gulp.series('build', 'resize'));
